94341f65586afcb36155b300f042dae9848ace62
Om <dreamcodelife@gmail.com>
2020-03-01 18:59:57 -0500

    First Blank Project

---
+++ CodeKataSolutions/ArrayProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArrayProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Array Problems!");
+
+            Console.ReadLine();
+        }
+    }
+}
