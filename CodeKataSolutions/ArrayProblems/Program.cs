564dc7517a4dc081d0ca14977205cefd21ca91dd
Om Ganesh <dreamcodelife@gmail.com>
2020-03-01 19:35:54 -0500

    Add the LowerBoundSTL solution

--- CodeKataSolutions/ArrayProblems/Program.cs
+++ CodeKataSolutions/ArrayProblems/Program.cs
@@ -8,7 +8,24 @@ namespace ArrayProblems
         {
             Console.WriteLine("Hello Array Problems!");
 
+            //Problem1: LowerBoundSTL
+            RunLowerBoundSTL();
+
             Console.ReadLine();
+        }
+
+
+        static void RunLowerBoundSTL()
+        {
+            //LowerBoundSTL Problem
+            int n = 8;
+            int[] nArray = { 1,1,2,3,6,9,9,15};
+            int q = 4;
+            int[] qArray = { 1,4,9,15};
+
+            LowerBoundSTL lBoundSTL = new LowerBoundSTL(n, nArray, q, qArray);
+            lBoundSTL.Execute();
+            lBoundSTL.ShowResult();
         }
     }
 }
