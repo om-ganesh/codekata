6f708902a36576380699c3ff31c528410879c7fd
Om Ganesh <dreamcodelife@gmail.com>
2020-03-11 23:20:09 -0400

    Compiled the CSharp project successfully

---
+++ csharpsolutions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace csharpsolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Challenges");
+
+            IProblem problem = new DynamicArray();
+            problem.Execute();
+            problem.ShowResult();
+        }
+
+    }
+}
