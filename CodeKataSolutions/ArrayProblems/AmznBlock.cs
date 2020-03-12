5a22746889ffc6b02bebf07e9f96e993cc567f22
Om Ganesh <dreamcodelife@gmail.com>
2020-03-11 20:49:42 -0400

    Provide solution for block amazon interview question (#6)
    * Provide solution for block amazon interview question
    
    * Include various test cases
    
    Co-authored-by: Om Ganesh <omganesh@gmail.com>

---
+++ CodeKataSolutions/ArrayProblems/AmznBlock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems
+{
+    class AmznBlock
+    {
+        public static int Execute(int[] inputArray)
+        {
+            int qty = 0;
+            // Trim away the leading and trailing 0s from input array
+            int[] arr = inputArray.Trim();
+            int start = arr[0];
+            int end = GetMyClosing(0, arr);
+            for(int i =1; i< arr.Length-1; i++)
+            {
+                if(arr[i]==end)
+                {
+                    start = arr[i];
+                    end = GetMyClosing(i, arr);
+                }
+                else
+                {
+                    qty += (start<end?start:end)- arr[i];
+                }
+                if (end == 0)
+                    break;
+            }
+
+            return qty;
+        }
+
+        private static int GetMyClosing(int index, int[] arr)
+        {
+            int myClosing = 0;
+            for(int i =index+1; i<arr.Length;i++)
+            {
+                if (arr[i] >= arr[index])
+                    return arr[i];
+                
+                if (arr[i] > myClosing)
+                    myClosing = arr[i];
+            }
+            return myClosing;
+        }
+    }
+}
---
+++ CodeKataSolutions/ArrayProblems/ArrayExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems
+{
+    static class ArrayExtensions
+    {
+        public static int[] Trim(this int[] arr)
+        {
+            int startIndex = 0;
+            int endIndex = arr.Length-1;
+            bool foundstart = false;
+            bool foundend = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (foundstart && foundend)
+                    break;
+                if (!foundstart && arr[i] != 0)
+                {
+                    startIndex = i;
+                    foundstart = true;
+                }
+                    
+                if (!foundend && arr[arr.Length - i - 1] != 0)
+                {
+                    endIndex = arr.Length - i - 1;
+                    foundend = true;
+                }
+            }
+            List<int> result = new List<int>();
+            for(int i=startIndex;i<=endIndex;i++)
+            {
+                result.Add(arr[i]);
+            }
+            return result.ToArray();
+        }
+    }
+}
