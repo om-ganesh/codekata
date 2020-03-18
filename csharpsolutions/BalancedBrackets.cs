7a2e79bc46349f75a239fd8554ccb96e39539a26
Om Ganesh <omganesh@gmail.com>
2020-03-18 02:04:51 -0400

    Updated to better solution

--- csharpsolutions/BalancedBrackets.cs
+++ csharpsolutions/BalancedBrackets.cs
@@ -7,9 +7,6 @@ namespace csharpsolutions
 {
     class BalancedBrackets: IProblem
     {
-        char[] openingBrackets = new char[]{ '{', '[', '(' };
-        char[] closingBrackets = new char[]{ '}', ']', ')' };
-
         string[] data;
         bool[] result;
         public BalancedBrackets()
@@ -34,39 +31,71 @@ namespace csharpsolutions
         }
 
         // Complete the isBalanced function below.
-        Boolean isBalanced(string str) {
+        Boolean isBalanced(string s) {
 
             Stack<char> stack = new Stack<char>();
-            //Corner Cases that can be avoided initially
-            var s = str.Trim();
-            if(s.Length == 0 || s.Length%2 ==1 
-                || closingBrackets.Contains(s[0]) || openingBrackets.Contains(s[s.Length-1]))
-            {
-                return false;
-            }
-        
             for(int j=0; j< s.Length; j++)
             {
-                char current = s[j];
-                
-                if(openingBrackets.Contains(current))
+                switch(s[j])
                 {
-                    stack.Push(current);
-                }
-                else if(closingBrackets.Contains(current))
-                {
-                    if(stack.Count==0 || Array.IndexOf(openingBrackets, stack.Pop()) != Array.IndexOf(closingBrackets, current))
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    return false;
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    default:
+                        if(stack.Count ==0 || s[j]!=stack.Pop())
+                        {
+                            return false;
+                        }
+                        break;
                 }
             }
+
             return stack.Count==0;
         }
+
+        // Complete the isBalanced function below.
+        //Boolean isBalancedOld(string str) {
+
+        //    char[] openingBrackets = new char[]{ '{', '[', '(' };
+        //    char[] closingBrackets = new char[]{ '}', ']', ')' };
+        //    Stack<char> stack = new Stack<char>();
+            
+        //    //Corner Cases that can be avoided initially
+        //    var s = str.Trim();
+        //    if(s.Length == 0 || s.Length%2 ==1 
+        //        || closingBrackets.Contains(s[0]) || openingBrackets.Contains(s[s.Length-1]))
+        //    {
+        //        return false;
+        //    }
+        
+        //    for(int j=0; j< s.Length; j++)
+        //    {
+        //        char current = s[j];
+                
+        //        if(openingBrackets.Contains(current))
+        //        {
+        //            stack.Push(current);
+        //        }
+        //        else if(closingBrackets.Contains(current))
+        //        {
+        //            if(stack.Count==0 || Array.IndexOf(openingBrackets, stack.Pop()) != Array.IndexOf(closingBrackets, current))
+        //            {
+        //                return false;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return stack.Count==0;
+        //}
 
         public void ShowResult()
         {
@@ -77,3 +106,38 @@ namespace csharpsolutions
         }
     }
 }
+
+//static string isBalanced1(string str) {
+
+//        char[] openingBrackets = new char[]{ '{', '[', '(' };
+//        char[] closingBrackets = new char[]{ '}', ']', ')' };
+//        Stack<char> stack = new Stack<char>();
+//        //Corner Cases that can be avoided initially
+//        var s =str.Trim();
+//        if(s.Length == 0 || s.Length%2 ==1 
+//            || closingBrackets.Contains(s[0]) || openingBrackets.Contains(s[s.Length-1]))
+//        {
+//            return "NO";
+//        }
+    
+//        for(int j=0; j< s.Length; j++)
+//        {
+//            char current = s[j];
+//            if(openingBrackets.Contains(current))
+//            {
+//                stack.Push(current);
+//            }
+//            else if(closingBrackets.Contains(current))
+//            {
+//                if( stack.Count==0 || Array.IndexOf(openingBrackets, stack.Pop()) != Array.IndexOf(closingBrackets, current))
+//                {
+//                    return "NO";
+//                }
+//            }
+//            else
+//                {
+//                    return "NO";
+//                }
+//        }
+//        return stack.Count==0? "YES" : "NO";
+//    }
