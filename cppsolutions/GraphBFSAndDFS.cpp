4e8456d66e46e73817e9266776400594d2605f11
Mahmud Hasan <hasanpoet@gmail.com>
2020-04-08 09:40:29 -0400

    BFS Completed CPP

--- cppsolutions/GraphBFSAndDFS.cpp
+++ cppsolutions/GraphBFSAndDFS.cpp
@@ -19,6 +19,8 @@ private:
 		{0, 0, 0, 0, 1, 0, 0} 
 	};
 
+	vector<int> statusVector{1, 1, 1, 1, 1, 1, 1};
+
 	void traverseNodesByBFS()
 	{
 		queue<int> visited;
@@ -27,7 +29,45 @@ private:
 		int startingNode = 0;
 
 		visited.push(startingNode);
+		statusVector[startingNode] = 3;
 
+		for (int i = 0; i < graphByAdjacentMatrix.size(); i++)
+		{
+			if (graphByAdjacentMatrix[startingNode][i] == 1)
+			{
+				adjacent.push(i);
+				statusVector[i] = 2;
+			}
+		}
+
+		while (!adjacent.empty())
+		{
+			int node = adjacent.front();
+			visited.push(node);
+			statusVector[node] = 3;
+			adjacent.pop();
+
+			for (int i = 0; i < graphByAdjacentMatrix.size(); i++)
+			{
+				if (graphByAdjacentMatrix[node][i] == 1 && statusVector[i] == 1)
+				{
+					adjacent.push(i);
+					statusVector[i] = 2;
+				}
+			}
+		}
+
+		printBFSOrder(visited);
+	}
+
+	void printBFSOrder(queue<int> traversedNodes)
+	{
+		while (!traversedNodes.empty())
+		{
+			cout << traversedNodes.front() << " ";
+			traversedNodes.pop();
+		}
+		cout << endl;
 	}
 
 	void traverseNodesByDFS()
