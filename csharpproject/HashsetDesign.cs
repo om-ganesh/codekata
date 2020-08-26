b039452dce1696e951c6b94b4e4dcbfa7c2b8f9a
Om Ganesh <dreamcodelife@gmail.com>
2020-08-25 21:15:34 -0400

    WIP-hashset skeleton class

---
+++ csharpproject/HashsetDesign.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace csharpproject
+{
+    /**
+     * Your MyHashSet object will be instantiated and called as such:
+     * MyHashSet obj = new MyHashSet();
+     * obj.Add(key);
+     * obj.Remove(key);
+     * bool param_3 = obj.Contains(key);
+     */
+
+    public class MyHashSet
+    {
+
+        /** Initialize your data structure here. */
+        public MyHashSet()
+        {
+
+        }
+
+        public void Add(int key)
+        {
+
+        }
+
+        public void Remove(int key)
+        {
+
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+            return false;
+        }
+    }
+}
