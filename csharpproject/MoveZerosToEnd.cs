4a098f6960a0b866a93eb496dcc67ed446bae6f6
Om Ganesh <dreamcodelife@gmail.com>
2020-12-27 20:20:08 -0500

    Solve the move zeros to end problem

---
+++ csharpproject/MoveZerosToEnd.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpproject
+{
+    class MoveZerosToEnd : IProblem
+    {
+        public void Execute()
+        {
+            int[] arr = new int[] { 5,0, 1, 0, 3, 12 };
+            Console.WriteLine($"Before Moving: {string.Join(",", arr)}");
+            MoveZeroes(arr);
+            Console.WriteLine($"After Moving: {string.Join(",", arr)}");
+        }
+
+
+        public void MoveZeroes(int[] nums)
+        {
+            int cnt = 0;
+            int i = 0;
+            while (i + cnt < nums.Length)
+            {
+                while (i + cnt < nums.Length && nums[i + cnt] == 0)
+                {
+                    cnt++;
+                }
+                if (i + cnt >= nums.Length)
+                {
+                    break;
+                }
+                nums[i] = nums[i + cnt];
+                i++;
+            }
+            while (i < nums.Length)
+            {
+                nums[i] = 0;
+                i++;
+            }
+        }
+    }
+}
