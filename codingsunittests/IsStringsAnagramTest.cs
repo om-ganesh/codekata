99b8b955df4b7251308934dd6cd1a022f62a6463
Om Ganesh <dreamcodelife@gmail.com>
2021-01-23 09:19:02 -0500

    Adding a unit test projects to solution

---
+++ codingsunittests/IsStringsAnagramTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using interviewcodings2018;
+using NUnit.Framework;
+
+namespace codingsunittests
+{
+    [TestFixture]
+    public class IsStringsAnagramTest
+    {
+        [Test]
+        public void AnagramWithDifferentWord()
+        {
+            String source = "test";
+            String target = "tests";
+
+            Assert.IsFalse(IsStringsAnagram.IsAnagram(source, target));
+        }
+
+        [Test]
+        public void AnagramWithCapitalize()
+        {
+            String source = "TEST";
+            String target = "sett";
+
+            Assert.IsTrue(IsStringsAnagram.IsAnagram(source, target));
+        }
+
+        [Test]
+        public void AnagramWithSingleCharacter()
+        {
+            String source = "h";
+            String target = "H";
+
+            Assert.IsTrue(IsStringsAnagram.IsAnagram(source, target));
+        }
+
+        [Test]
+        public void AnagramWithLongString()
+        {
+            String source = "Welcometothewholelongstring";
+            String target = "longstringtothewholewelcome";
+
+            Assert.IsTrue(IsStringsAnagram.IsAnagram(source, target));
+        }
+    }
+}
---
+++ codingsunittests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("codingsunittests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("codingsunittests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb26064d-178f-48d7-bca3-fb8cb83e055a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
