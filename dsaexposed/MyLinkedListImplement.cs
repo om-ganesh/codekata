83da90859e7822886118e97799be2f4d113ceb02
Om Ganesh <dreamcodelife@gmail.com>
2021-04-03 20:23:18 -0400

    Create the linkedlist implementation Port the WIP Graph related works

--- dsaexposed/MyLinkedListImplement.cs
+++ dsaexposed/MyLinkedListImplement.cs
@@ -9,8 +9,15 @@ namespace dsaexposed
     {
         public static void Init()
         {
+            //Initialize LinkedList
             MyLinkedList<Int32> linkedList = new MyLinkedList<Int32>();
             Console.WriteLine("Linked List : Initial Stage");
+            linkedList.PrintAll();
+
+            //Add array elements to LinkedList
+            Console.WriteLine("Linked List : Add array elements");
+            Node<int> headNode = MyLinkedList<int>.CreateInstance(new int[] { 90, 80, 70 });
+            linkedList.AddNode(headNode);
             linkedList.PrintAll();
 
             Console.WriteLine("Linked List : Add a element");
@@ -22,11 +29,11 @@ namespace dsaexposed
             linkedList.AddNode(new Node<int>(11));
             linkedList.PrintAll();
 
-            Console.WriteLine("Try to remove not available element");
-            linkedList.RemoveNode(new Node<int>(23));
-            Console.WriteLine("Try to remove in between/last element");
-            linkedList.RemoveNode(new Node<int>(11));
-            linkedList.PrintAll();
+            //Console.WriteLine("Try to remove not available element");
+            //linkedList.RemoveNode(new Node<int>(23));
+            //Console.WriteLine("Try to remove in between/last element");
+            //linkedList.RemoveNode(new Node<int>(11));
+            //linkedList.PrintAll();
 
             Console.WriteLine("After removing duplicates");
             linkedList.RemoveDuplicates();
@@ -34,9 +41,13 @@ namespace dsaexposed
             linkedList.PrintAll();
 
 
-            Console.WriteLine("Finding the middle of linked list");
-            Node<int> middle = linkedList.FindMiddle();
-            Console.WriteLine($"The middle element is {middle.Data}");
+            Console.WriteLine("Reverse the linked list");
+            linkedList.Reverse();
+            linkedList.PrintAll();
+
+            //Console.WriteLine("Finding the middle of linked list");
+            //Node<int> middle = linkedList.FindMiddle();
+            //Console.WriteLine($"The middle element is {middle.Data}");
         }
     }
 }
