4d206066d7e97d3197222964dd80610e223ffa30
Om Ganesh <dreamcodelife@gmail.com>
2021-04-22 08:52:48 -0400

    Minor fix some tree problems

--- dsafundamentals/tree/Node.cs
+++ dsafundamentals/tree/Node.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace tree
 {
@@ -11,17 +12,174 @@ namespace tree
 
         public Node Parent { get; set; }
 
-        public Node()
+        public Node(int Data=0)
         {
-
+            this.Data = Data;
             Left = null;
             Right = null;
         }
 
-        public Node(int Data) : this()
+        public void Traverse(TreeTraversal dir = TreeTraversal.InTraversal)
         {
-            this.Data = Data;
+            Console.Write($"{Environment.NewLine}{dir} Traversal: ");
+            switch (dir)
+            {
+                case TreeTraversal.PreTraversal:
+                    PreTraversal(this);
+                    break;
+                case TreeTraversal.PreTraversal_Iterative:
+                    PreTraversalIterative(this);
+                    break;
+                case TreeTraversal.PostTraversal:
+                    PostTraversal(this);
+                    break;
+                case TreeTraversal.BFSTraversal:
+                    BFSTraversal(this);
+                    break;
+                case TreeTraversal.BFSTraversalWithLabel:
+                    BFSTraversalWithLevel(this);
+                    break;
+                default:
+                    InTraversal(this);
+                    break;
+            }
         }
 
+        private void InTraversal(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            InTraversal(node.Left);
+            Visit(node);
+            InTraversal(node.Right);
+        }
+
+        private void PreTraversal(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Visit(node);
+            InTraversal(node.Left);
+            InTraversal(node.Right);
+        }
+
+        private void PostTraversal(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            InTraversal(node.Left);
+            InTraversal(node.Right);
+            Visit(node);
+        }
+
+        private void PreTraversalIterative(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(node);
+            while (stack.Count > 0)
+            {
+                Node current = stack.Pop();
+                Visit(current);
+                if (current.Right != null)
+                    stack.Push(current.Right);
+                if (current.Left != null)
+                    stack.Push(current.Left);
+            }
+        }
+
+        private void BFSTraversal(Node root)
+        {
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                Node current = q.Dequeue();
+                Visit(current);
+                if (current.Left != null)
+                {
+                    q.Enqueue(current.Left);
+                }
+                if (current.Right != null)
+                {
+                    q.Enqueue(current.Right);
+                }
+            }
+        }
+
+        private void BFSTraversalWithLevel(Node root)
+        {
+            SetLevels(root, 1);
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(root);
+            int currentLevel = 1;
+            while (q.Count > 0)
+            {
+                Node current = q.Dequeue();
+                if (current.Level > currentLevel)
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    currentLevel++;
+                }
+                Visit(current);
+                if (current.Left != null)
+                {
+                    q.Enqueue(current.Left);
+                }
+                if (current.Right != null)
+                {
+                    q.Enqueue(current.Right);
+                }
+            }
+        }
+
+        public void Visualize(int indent = 0, char child = '0')
+        {
+            // Print me
+            Console.WriteLine($"{new string(' ', indent * 2)}{child}:{this.Data}");
+            if (this.Left != null)
+            {
+                this.Left.Visualize(indent + 1, 'L');
+            }
+            if (this.Right != null)
+            {
+                this.Right.Visualize(indent + 1, 'R');
+            }
+        }
+
+        private void SetLevels(Node root, int i)
+        {
+            if (root == null)
+                return;
+            root.Level = i;
+            if (root.Left != null)
+                SetLevels(root.Left, i + 1);
+            if (root.Left != null)
+                SetLevels(root.Right, i + 1);
+        }
+
+        private void Visit(Node root)
+        {
+            Console.Write(root.Data + "-");
+        }
+    }
+
+    public enum TreeTraversal
+    {
+        PreTraversal,
+        InTraversal,
+        PostTraversal,
+        PreTraversal_Iterative,
+        BFSTraversal,
+        BFSTraversalWithLabel
     }
 }
---
+++ dsafundamentals/tree/TreeHelper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tree
+{
+    public class TreeHelper
+    {
+        /// <summary>
+        /// Creates Binary Tree from array, root at middle and splitting recursively in two halves
+        /// </summary>
+        /// <returns></returns>
+        public static Node CreateBinaryTree(int[] arr)
+        {
+            return CreateBT(arr, 0, arr.Length);
+        }
+
+        private static Node CreateBT(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                Node root = new Node(arr[mid]);
+                root.Left = CreateBT(arr, left, mid);
+                root.Right = CreateBT(arr, mid + 1, right);
+                return root;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Create Binary Search Tree (starting from root as array[0])
+        /// </summary>
+        /// <returns></returns>
+        public static Node CreateBinarySearchTree(int[] data)
+        {
+            if (data.Length < 1)
+            {
+                return null;
+            }
+            Node root = new Node(data[0]);
+            for (int i = 1; i < data.Length; i++)
+            {
+                InsertInBST(root, data[i]);
+            }
+            return root;
+        }
+
+        private static void InsertInBST(Node node, int data)
+        {
+            if (data > node.Data)
+            {
+                if (node.Right != null)
+                {
+                    InsertInBST(node.Right, data);
+                }
+                else
+                {
+                    node.Right = new Node(data);
+                }
+            }
+            else
+            {
+                if (node.Left != null)
+                {
+                    InsertInBST(node.Left, data);
+                }
+                else
+                {
+                    node.Left = new Node(data);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create Tree (Starting from root as array[0])
+        /// </summary>
+        /// <returns></returns>
+        public static Node CreateTree(int[] arr, int index=0, Node current=null)
+        {
+            if (index < arr.Length)
+            {
+                current = new Node(arr[index]);
+                current.Left = CreateTree(arr, 2 * index + 1, current.Left);
+                current.Right = CreateTree(arr, 2 * index + 2, current.Right);
+            }
+            return current;
+        }
+
+        public static bool Search(Node tree, int val)
+        {
+            if (tree == null)
+            {
+                return false;
+            }
+            if (tree.Data == val)
+            {
+                return true;
+            }
+            else if (val > tree.Data)
+            {
+                Search(tree.Right, val);
+            }
+            else
+            {
+                Search(tree.Left, val);
+            }
+            return false;
+        }
+    }
+}
