ccb3d332bb592937840c3c9b828e4b8d694c907e
Om Ganesh <dreamcodelife@gmail.com>
2021-04-22 08:54:09 -0400

    Combine and redo some tree problems (#90)
    -Tree
    -BinaryTree
    -BinarySearchTree
    -Traversal (breadth first)
    -Traversal (Depth in/pre/post)
    -Searching

--- dsafundamentals/tree/Program.cs
+++ dsafundamentals/tree/Program.cs
@@ -7,38 +7,74 @@ namespace tree
     {
         static void Main(string[] args)
         {
+            // Display all employee names from top-down organizational chart.
+            // Also show Level of each employee (assuming root to be "1")
             Console.WriteLine("@HelloGybe Interview!!!");
-            ShowTreeChildrenBreadthWise();
+            TrieNode company = CreateCompanyTree();
+            company.ShowAll();
 
             Console.WriteLine("@IBM Interview!!!");
-            TreeTraversal traversal = new TreeTraversal();
-            traversal.Init();
-            traversal.Init(true); //Create a binary search tree
+            int[] arr = { 12, 6, 127, 28, 9, 3, 111, 0 };
+            VariousTreeOperations(arr);
 
             Console.Read();
         }
 
-        public static void ShowTreeChildrenBreadthWise()
+        public static void VariousTreeOperations(int[] arr)
         {
-            TreeNode rootNode = new TreeNode("Company", null, null);
-            List<TreeNode> rootChild = new List<TreeNode>
-            {
-                new TreeNode("Haris", "CEO", rootNode),
-                new TreeNode("James", "CTO", rootNode),
-                new TreeNode("Sahas", "CFO", rootNode)
-            };
-            rootNode.Children = rootChild;
 
-            CompanyTree companyTree = new CompanyTree(rootNode);
+            Console.WriteLine($"Binary Tree Example");
+            Node binaryTree = TreeHelper.CreateBinaryTree(arr);
+            binaryTree.Visualize();
+            binaryTree.Traverse(TreeTraversal.InTraversal);
+            binaryTree.Traverse(TreeTraversal.PreTraversal);
+            binaryTree.Traverse(TreeTraversal.PostTraversal);
+            binaryTree.Traverse(TreeTraversal.BFSTraversal);
+            //binaryTree.Traverse(TreeTraversal.BFSTraversalWithLabel);
 
-            List<TreeNode> ceoChild = new List<TreeNode>
-            {
-                new TreeNode("Subash", "Manager", rootNode.Children[0], null),
-                new TreeNode("Gita", "Marketing", rootNode.Children[0], null),
-            };
-            rootNode.Children[0].Children = ceoChild;
+            Console.WriteLine($"{Environment.NewLine}Binary Search Tree Example");
+            Node binarySearchTree = TreeHelper.CreateBinarySearchTree(arr);
+            binarySearchTree.Visualize();
+            binarySearchTree.Traverse(TreeTraversal.InTraversal);
+            binarySearchTree.Traverse(TreeTraversal.PreTraversal_Iterative);
+            binarySearchTree.Traverse(TreeTraversal.PostTraversal);
+            binarySearchTree.Traverse(TreeTraversal.BFSTraversal);
 
-            companyTree.ShowAll(rootNode);
+            Console.WriteLine($"{Environment.NewLine}Tree Example");
+            Node tree = TreeHelper.CreateTree(arr);
+            tree.Visualize();
+            tree.Traverse(TreeTraversal.InTraversal);
+            tree.Traverse(TreeTraversal.PreTraversal);
+            tree.Traverse(TreeTraversal.PostTraversal);
+            tree.Traverse(TreeTraversal.BFSTraversal);
+            //tree.Traverse(TreeTraversal.BFSTraversalWithLabel);
+
+            Console.WriteLine($"{Environment.NewLine}Search if the element {45} exists in Tree? {TreeHelper.Search(tree, 415)}");
+        }
+
+        public static TrieNode CreateCompanyTree()
+        {
+            TrieNode rootNode = new TrieNode("Company");
+            rootNode.AddChildren(new List<TrieNode>
+                {
+                    new TrieNode("Haris", "CEO"),
+                    new TrieNode("James", "CTO"),
+                    new TrieNode("Sahas", "CFO")
+                });
+
+            rootNode.Children[1].AddChildren(new List<TrieNode>
+                {
+                    new TrieNode("Kul", "Security Manager"),
+                    new TrieNode("Umesh", "Architect"),
+                });
+
+            rootNode.Children[0].AddChildren(new List<TrieNode>
+                {
+                    new TrieNode("Subash", "Manager"),
+                    new TrieNode("Gita", "Marketing"),
+                });
+
+            return rootNode;
         }
     }
 }
---
+++ dsafundamentals/tree/TrieNode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tree
+{
+    public class TrieNode
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public TrieNode Parent { get; set; }
+        public List<TrieNode> Children { get; set; }
+
+        public TrieNode(string name, string title=null)
+        {
+            Name = name;
+            Title = title;
+            Parent = null;
+            Children = new List<TrieNode>();
+        }
+
+        public void AddChildren(List<TrieNode> childs)
+        {
+            foreach(var child in childs)
+            {
+                child.Parent = this;
+                this.Children.Add(child);
+            }
+        }
+
+        public void ShowAll()
+        {
+            TrieNode first = this;
+            Queue<TrieNode> names = new Queue<TrieNode>();
+            names.Enqueue(first);
+
+            int level = 0;
+            while (names.Count > 0)
+            {
+                int childAtThisLevel = names.Count;
+                while (childAtThisLevel-- != 0)
+                {
+                    TrieNode current = names.Dequeue();
+                    Console.WriteLine($"{new string(' ', level * 4)}L{level + 1}:{current.Name}");
+                    current?.Children?.ForEach(c => names.Enqueue(c));
+                }
+                level++;
+            }
+        }
+    }
+}
